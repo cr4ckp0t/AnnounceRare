e5e8de7f12c788b10c32b269983efcfb13e62d38
Adam Koch <me@adamko.ch>
2022-05-31 21:21:13 -0400

    Update TOC for 9.2.5

---
+++ PushTags.ps1
@@ -0,0 +1,26 @@
+# Determine Version and Do A Git Push w/ Tags
+
+$addonName = Split-Path $PSScriptRoot -Leaf
+$tocFile = "{0}\{1}.toc" -f ($PSScriptRoot, $addonName)
+
+if (Test-Path -Path $tocFile) {
+    $contents = Get-Content -Path $tocFile
+
+    # determine the version line
+    for ($i = 0; $i -lt $contents.Count; $i++) {
+        if ($contents[$i] -match "Version") {
+            break;
+        }
+    }
+    # .*_(\d+(\.\d+){1,3})
+    if ($contents[$i] -match "## Version: (\d+\.\d+\.\d+)") {
+        git tag -a $Matches[1] -m ("{0} Release" -f $Matches[1])
+        git push origin --tags
+    }
+    else {
+        Write-Host "Unable to determine addon version."
+    }
+}
+else {
+    Write-Host "Failed to determine TOC file."
+}
